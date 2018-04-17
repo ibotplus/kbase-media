870ba009e138e0f3458c5f18ecb64d7a98cb84b8
yogurt_lei <yogurt_lei@foxmail.com>
2018-04-17 15:25:51 +0800

    引入mq 待优化

--- src/main/java/com/eastrobot/converter/Application.java
+++ src/main/java/com/eastrobot/converter/Application.java
@@ -1,24 +1,16 @@
 package com.eastrobot.converter;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
-@Slf4j
 @SpringBootApplication
 @EnableConfigurationProperties
 public class Application {
-
-    @Value("${convert.outputFolder}")
-    private String OUTPUT_FOLDER;
-
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(Application.class);
-        application.run(args);
+        new SpringApplication(Application.class).run(args);
     }
 
     /**
---
+++ src/main/java/com/eastrobot/converter/plugin/RocketMQProducer.java
@@ -0,0 +1,69 @@
+package com.eastrobot.converter.plugin;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * RocketMQProducer
+ *
+ * @author <a href="yogurt_lei@foxmail.com">Yogurt_lei</a>
+ * @version v1.0 , 2018-04-17 11:39
+ */
+@Slf4j
+@Component
+public class RocketMQProducer {
+    /**
+     * 生产者的组名
+     */
+    @Value("${apache.rocketmq.producer.producerGroup}")
+    private String producerGroup;
+
+    /**
+     * NameServer 地址
+     */
+    @Value("${apache.rocketmq.namesrvAddr}")
+    private String namesrvAddr;
+
+    @PostConstruct
+    public void defaultMQProducer() {
+        //生产者的组名
+        DefaultMQProducer producer = new DefaultMQProducer(producerGroup);
+        //指定NameServer地址，多个地址以 ; 隔开
+        producer.setNamesrvAddr(namesrvAddr);
+
+        try {
+            /**
+             * Producer对象在使用之前必须要调用start初始化，初始化一次即可
+             * 注意：切记不可以在每次发送消息时，都调用start方法
+             */
+            producer.start();
+
+            //创建一个消息实例，包含 topic、tag 和 消息体
+            //如下：topic 为 "TopicTest"，tag 为 "push"
+            Message message = new Message("TopicTest", "push", "发送消息----zhisheng-----".getBytes(RemotingHelper
+                    .DEFAULT_CHARSET));
+
+            StopWatch stop = new StopWatch();
+            stop.start();
+
+            for (int i = 0; i < 10000; i++) {
+                SendResult result = producer.send(message);
+                System.out.println("发送响应：MsgId:" + result.getMsgId() + "，发送状态:" + result.getSendStatus());
+            }
+            stop.stop();
+            System.out.println("----------------发送一万条消息耗时：" + stop.getTotalTimeMillis());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            producer.shutdown();
+        }
+    }
+}
