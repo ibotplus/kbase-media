7472bb28f8358fe606e297efb570b2c4de9d7124
yogurt_lei <yogurt_lei@foxmail.com>
2018-06-27 15:27:59 +0800

    optimize code

--- src/main/java/com/eastrobot/converter/Application.java
+++ src/main/java/com/eastrobot/converter/Application.java
@@ -1,16 +1,15 @@
 package com.eastrobot.converter;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.DispatcherServlet;
 
-@Slf4j
-@SpringBootApplication(exclude = {MultipartAutoConfiguration.class})
 @EnableConfigurationProperties
+@SpringBootApplication(exclude = {MultipartAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
         new SpringApplication(Application.class).run(args);
@@ -19,8 +18,13 @@ public class Application {
     /**
      * 替换默认文件上传实现StandardServletMultipartResolver
      */
-    @Bean("multipartResolver")
+    @Bean(DispatcherServlet.MULTIPART_RESOLVER_BEAN_NAME)
     public CommonsMultipartResolver multipartResolver() {
-        return new CommonsMultipartResolver();
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+        // 上传不受限 由逻辑控制
+        resolver.setMaxInMemorySize(-1);
+        resolver.setMaxUploadSizePerFile(-1);
+
+        return resolver;
     }
 }
