de7747e2e4bca9f758528016f86895cae9e82d48
shun.yang <shun.yang@xiaoi.com>
2018-07-09 18:41:29 +0800

    tts 接口长文本截取

--- src/main/java/com/eastrobot/converter/service/impl/AudioServiceImpl.java
+++ src/main/java/com/eastrobot/converter/service/impl/AudioServiceImpl.java
@@ -8,15 +8,17 @@ import com.eastrobot.converter.util.shhan.ShhanAsrUtil;
 import com.eastrobot.converter.util.xfyun.XfyunAsrConstants;
 import com.eastrobot.converter.util.xfyun.XfyunAsrUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import sun.misc.BASE64Encoder;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import static com.eastrobot.converter.model.ResultCode.*;
 import static com.eastrobot.converter.util.baidu.BaiduAsrConstants.PCM;
@@ -59,17 +61,54 @@ public class AudioServiceImpl implements AudioService {
     @Override
     public ParseResult handleTts(String text) {
         if(StringUtils.isNoneBlank(text)){
-            byte[] data = baiduTtsHandler(text);
+            byte[] data = null;
+            // 截取
+             if(text.length()>512){//需截取
+                List<String> splitList= splitText(text);
+                 for (String sText : splitList) {
+                     byte[] bytes = baiduTtsHandler(sText);
+                     if(data==null){
+                         data = bytes;
+                     }else if(data!=null&&bytes!=null){
+                         byte[] copy = ArrayUtils.addAll(data, bytes);
+                         data = copy;
+                     }
+                 }
+            }else{
+                data = baiduTtsHandler(text);
+            }
             if(data!=null){
-                //对字节数组Base64编码
-                BASE64Encoder encoder = new BASE64Encoder();//encoder.encode(data)
-                return new ParseResult(SUCCESS, SUCCESS.getMsg(), "", "",data);
+                  return new ParseResult(SUCCESS, SUCCESS.getMsg(), "", "",data);
             }else{
                 return new ParseResult(PARSE_EMPTY, PARSE_EMPTY.getMsg(), "", "",null);
             }
          }else {
             return new ParseResult(TTS_FAILURE, "文本内容为空", "", "",null);
         }
+    }
+
+    /**
+     * 根据长度500和句号进行混合截取
+     * @param text
+     * @return
+     */
+    private List<String> splitText(String text) {
+        ArrayList<String> result = new ArrayList<String>();
+        while (text.length()>500){
+            String value = text.substring(0,Math.min(500,text.length()));
+            text = text.substring(500);
+            if(!value.endsWith("。")){//
+                value = value + text.substring(0,text.indexOf("。")+1);
+                text = text.substring(text.indexOf("。")+1);
+            }
+            result.add(value);
+        }
+
+        if (text.length()<100){
+            result.add(text);
+        }
+
+        return result;
     }
 
     private String baiduAsrHandler(String audioFilePath) throws Exception {
@@ -106,6 +145,7 @@ public class AudioServiceImpl implements AudioService {
         String lan="zh";// 固定值zh。语言选择,目前只有中英文混合模式，填写固定值zh
         int ctp = 1; // 客户端类型选择，web端填写固定值1
         HashMap<String, Object> options = new HashMap<String, Object>();
+
         return BaiduAsrUtils.tts(tex,lan,ctp,options);
     }
 }
