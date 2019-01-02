659c0f82cda626ee047f752c33ba3fe9e56f97a1
yogurt_lei <yogurt_lei@foxmail.com>
2019-01-02 16:40:14 +0800

    feat: 提取xfyun config 接入xfxun web RESTful API

--- src/main/java/com/eastrobot/kbs/media/util/xfyun/XfyunAsrUtil.java
+++ src/main/java/com/eastrobot/kbs/media/util/xfyun/XfyunAsrUtil.java
@@ -1,80 +1,129 @@
 package com.eastrobot.kbs.media.util.xfyun;
 
 import com.eastrobot.kbs.media.model.Constants;
+import com.google.common.collect.ImmutableMap;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
- * IflytekAsrUtil
+ * XfyunAsrUtil
  *
  * @author <a href="yogurt_lei@foxmail.com">Yogurt_lei</a>
  * @version v1.0 , 2018-04-19 16:09
  */
 @Slf4j
 @Component
-@ConditionalOnProperty(prefix = "convert", name = "video.asr.default", havingValue = Constants.XFYUN)
+@ConditionalOnProperty(prefix = "convert", name = "audio.asr.default", havingValue = Constants.XFYUN)
 public class XfyunAsrUtil {
 
+    @Value("${convert.audio.asr.xfyun.appId}")
+    private String appId;
+    @Value("${convert.audio.asr.xfyun.apiKey}")
+    private String apiKey;
+    @Value("${convert.audio.asr.xfyun.apiUrl}")
+    private String apiUrl;
+
+    private static Config config;
+
+    @PostConstruct
+    private void init() {
+        config = new Config(appId, apiKey, apiUrl);
+        log.info("initialize xfyun asr tools complete.");
+    }
+
     /**
-     * {
-     * "bg":250,    \\当前这句话的说话开始时间，单位为毫秒
-     * "ed":2890,  \\当前这句话的说话结束时间，单位为毫秒
-     * "onebest":"噢，你，你听得到我这边的声音吗？",
-     * "speaker":"1" \\说话人编号（数字“1”和“2”为不同说话人，电话专用版功能）
-     * }﻿
-     * see http://www.xfyun.cn/doccenter/lfasr#go_sdk_doc_v2
+     * xf asr
+     * see https://doc.xfyun.cn/rest_api/%E8%AF%AD%E9%9F%B3%E5%90%AC%E5%86%99.html
+     *
+     * @param path 语音文件路径
      */
     public static JSONObject asr(String path) throws Exception {
-        final String url = "http://api.xfyun.cn/v1/service/v1/iat";
-        String apiKey = "da08f42480e67f574a61290717e8f945";
-        String appId = "5be241a0";
         //{"engine_type":"sms16k","aue":"raw"} 固定值
-        String paramBase64 = "eyJlbmdpbmVfdHlwZSI6ICJzbXMxNmsiLCJhdWUiOiJyYXcifQ==";
-        String currentTimeMillis = System.currentTimeMillis() / 1000L + "";
-        String md5Hex = DigestUtils.md5Hex((apiKey + currentTimeMillis + paramBase64).getBytes());
+        // 业务参数
+        String param = "{\"aue\":\"raw\",\"engine_type\":\"sms16k\"}";
+        String xParam = new String(Base64.encodeBase64(param.getBytes("UTF-8")));
+        // 系统当前秒数时间戳
+        String xCurTime = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()) + "";
+        // 生成令牌
+        String xCheckSum = DigestUtils.md5Hex(config.apiKey + xCurTime + xParam);
 
-        URL httpUrl = new URL(url);
-        HttpURLConnection conn = (HttpURLConnection) httpUrl.openConnection();
-        conn.setRequestMethod("POST");
-        conn.setRequestProperty("X-CurTime", currentTimeMillis);
-        conn.setRequestProperty("X-Param", paramBase64);
-        conn.setRequestProperty("X-Appid", appId);
-        conn.setRequestProperty("X-CheckSum", md5Hex);
-        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=utf-8");
-        // 设置请求 body
-        conn.setDoOutput(true);
-        conn.setDoInput(true);
+        // 内容base64
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        String audioBase64 = URLEncoder.encode("audio=" + new String(Base64.encodeBase64(bytes), "UTF-8"), "UTF-8");
 
-        //设置连接超时和读取超时时间
-        conn.setConnectTimeout(20000);
-        conn.setReadTimeout(20000);
-        conn.connect();
-        //POST请求
-        OutputStream out = conn.getOutputStream();
-        byte[] bytes = Base64.encodeBase64(Files.readAllBytes(Paths.get(path)));
-        String body = URLEncoder.encode(new String(bytes), "utf-8");
-        out.write(("audio=" + body).getBytes());
-        out.flush();
-        //读取响应
-        InputStream is = conn.getInputStream();
-        String result = IOUtils.toString(is, "utf-8");
-        out.close();
-        is.close();
-        conn.disconnect();
+        String result = postForResult(ImmutableMap.of(
+                "Content-Type", "application/x-www-form-urlencoded; charset=utf-8",
+                "X-CurTime", xCurTime,
+                "X-Param", xParam,
+                "X-Appid", config.appId,
+                "X-CheckSum", xCheckSum
+        ), audioBase64);
 
         return new JSONObject(result);
+    }
+
+    private static String postForResult(Map<String, String> header, String body) {
+        String result = "";
+        try {
+            URL httpUrl = new URL(config.apiUrl);
+            HttpURLConnection conn = (HttpURLConnection) httpUrl.openConnection();
+            conn.setRequestMethod("POST");
+
+            // 设置 header
+            for (Map.Entry<String, String> entry : header.entrySet()) {
+                String key = entry.getKey();
+                String value = entry.getValue();
+                conn.setRequestProperty(key, value);
+            }
+
+            // 设置请求 body
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+
+            //设置连接超时和读取超时时间
+            conn.setConnectTimeout(20000);
+            conn.setReadTimeout(20000);
+            conn.connect();
+
+            try (PrintWriter out = new PrintWriter(conn.getOutputStream());
+                 InputStream is = conn.getInputStream()) {
+                out.print(body);
+                out.flush();
+                result = IOUtils.toString(is, "utf-8");
+            } finally {
+                conn.disconnect();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    @AllArgsConstructor
+    private static class Config {
+        private String appId;
+        private String apiKey;
+        private String apiUrl;
     }
 }
