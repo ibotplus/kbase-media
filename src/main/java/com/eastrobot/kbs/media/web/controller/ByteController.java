ba21738b25a7bf7a12a0a63a9e8df00c6498270e
yogurt_lei <yogurt_lei@foxmail.com>
2019-10-24 17:17:20 +0800

    fix: change method to post

--- src/main/java/com/eastrobot/kbs/media/web/controller/ByteController.java
+++ src/main/java/com/eastrobot/kbs/media/web/controller/ByteController.java
@@ -7,10 +7,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ResponseHeader;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Api(tags = "转换接口(byte)")
 @Slf4j
@@ -22,7 +19,7 @@ public class ByteController {
     @ApiResponse(code = 200, message = "tts语音生成成功.", responseHeaders = {
             @ResponseHeader(name = "Content-disposition", description = "attachment; filename=download.mp3")
     })
-    @GetMapping(value = "/tts", produces = "audio/mp3")
+    @PostMapping(value = "/tts", produces = "audio/mp3")
     public ResponseEntity<byte[]> tts(@RequestParam String text) {
         return ResponseEntity.ok(DataBakerUtil.tts(text));
     }
